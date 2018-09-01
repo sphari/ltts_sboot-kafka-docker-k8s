ff73a78ea3eb4ea9fa6e46bd37946f28bd42ec07
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2018-09-01 13:13:27 +0530

    Sprint Boot Web examples

---
+++ labs/examples/SpringBootWebEx/src/main/java/com/examples/spring/boot/web/SpringBootGreetingsWebApp.java
@@ -0,0 +1,12 @@
+package com.examples.spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGreetingsWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGreetingsWebApp.class, args);
+	}
+}
---
+++ labs/examples/SpringBootWebEx/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Welcome to Spring Boot Web training :)
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+</body>
+</html>
