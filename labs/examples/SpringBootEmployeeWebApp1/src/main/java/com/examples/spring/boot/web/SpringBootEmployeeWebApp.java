4aaa78233c6233d55a1a835822ebf36d8129c1b8
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2018-09-01 17:53:00 +0530

    Spring Boot Employee Web App with DB integration

---
+++ labs/examples/SpringBootEmployeeWebApp1/src/main/java/com/examples/spring/boot/web/SpringBootEmployeeWebApp.java
@@ -0,0 +1,12 @@
+package com.examples.spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeWebApp.class, args);
+	}
+}
